0f7641462ff237d60a8ccd3bbc567a757e60aa06
painye <1850384745@qq.com>
2019-10-03 17:18:53 +0800

    将shelve存储的数据信息，解析到数据库中

--- test_1/dump_db_shelve.py
+++ test_1/dump_db_shelve.py
@@ -1,5 +1,9 @@
 import shelve
 
+'''
+    这里仍是一个字典的字典， 但最顶层的字典实际上是映射到文件的shelve，当通过键来访问记录时
+    其内部会使用pickle来序列化或反序列化记录，外面以一个通过键访问的文件作为接口。你也可以认为他就是一个持久化的字典'''
+
 db = shelve.open('people-shelve')
 for key in db:
     print(key, "=>\n", db[key])
