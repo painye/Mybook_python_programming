27f4884ac7e57d6d5247b45978d8a4ed063ac4b8
painye <1850384745@qq.com>
2019-10-04 16:32:57 +0800

    manager实例化会造成job对象的job参数冗余，所以重构构造函数，调用父类的构造函数直接将job传过去

--- test_1/manager.py
+++ test_1/manager.py
@@ -7,6 +7,9 @@ from person import Person
 
 
 class Manager(Person):
+    def __init__(self, name, age, pay):
+        Person.__init__(self, name, age, pay, 'manager')
+
     def giveRaise(self, percent, bonus=0.1):
         Person.giveRaise(self, percent+bonus)
 
