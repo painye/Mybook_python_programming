c4ea6a831aa081a96516ffcb788727541f4b3e7a
painye <1850384745@qq.com>
2019-10-07 15:01:16 +0800

    GUI的小测试，command属性中不传递参数，只写函数名不会对函数进行调用，只是指定了按钮的功能

--- test_1/GUI/tkinter101.py
+++ test_1/GUI/tkinter101.py
@@ -9,6 +9,7 @@ def reply():
 # 创建一个主控件
 window = Tk()
 # 创建一个按钮，标题press，功能是reply函数的功能
+# command中只给出了函数的名字，没有给所需要的函数参数所以不会调用只是指定了功能函数
 button = Button(window, text='press', command=reply)
 button.pack()
 window.mainloop()
