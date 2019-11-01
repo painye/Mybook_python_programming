f056e726aebdb934f34b2c175728743a376fd05e
painye <1850384745@qq.com>
2019-11-01 17:17:23 +0800

    初始的HTML输入表单，以及显示的获得结果和处理更新请求的Python CGI脚本

---
+++ test_1/CGI/cgi-bin/cgi101.py
@@ -0,0 +1,10 @@
+import cgi
+
+form = cgi.FieldStorage()   # 解析表单数据
+# hdr加回车
+print('Content-type: text/html\n')
+print('<title>Reply Page</title>')
+if not 'user' in form:
+    print('<h1>Who are you?</h1>')
+else:
+    print('<h1>Hello <i>%s</i>!</h1>' % cgi.escape(form['user'].value))
